87d506968b75653ce8709ff50c69cc8ae27b03b3
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-12-02 14:31:18 +0000

    Update indentation

--- scala/src/main/scala/example/Main.scala
+++ scala/src/main/scala/example/Main.scala
@@ -5,6 +5,6 @@ import example.day1.DayOne
 import scala.io.Source
 
 object Main extends App {
-//  println(DayOne.partOne(DayOne.input, 0))
-println(DayOne.partTwo(DayOne.input, 0))
+  println(DayOne.partOne(DayOne.input, 0))
+  println(DayOne.partTwo(DayOne.input, 0))
 }
